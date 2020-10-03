4fe694ebd8caf8f86da9dca80750ddd94d2b7373
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2020-10-03 17:51:11 -0400

    Fix import sorting/spacing

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.conf import settings
 
 
 @login_required(login_url=settings.LOGIN)
